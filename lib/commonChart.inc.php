0acb240c0c92b029b089f588a5d309cb9dfc4fb8
Fragner <frama1038@gmail.com>
2013-09-26 06:06:25 -0700

    add changes from berwinter/uvr1611: (rev66-68)  * support multiple elements for animation  * timezone and loop fix  * Added animation support

--- lib/commonChart.inc.php
+++ lib/commonChart.inc.php
@@ -7,6 +7,7 @@
  */
 include_once("lib/backend/uvr1611-connection.inc.php");
 include_once("lib/backend/database.inc.php");
+date_default_timezone_set("Europe/Berlin");
 
 // set json header
 header('Cache-Control: no-cache, must-revalidate');
@@ -46,8 +47,9 @@ if($date == date("Y-m-d") && ($database->lastDataset() + Config::getInstance()->
 	try {
 		$uvr = Uvr1611::getInstance();
 		$data = Array();
+		$count = $uvr->getCount();
 		$lastDatabaseValue = $database->lastDataset();
-		while($uvr->getCount()) {
+		for($i=0; $i < $count; $i++) {
 			// fetch a set of dataframes and insert them into the database
 			$value = $uvr->fetchData();
 			if(strtotime($value["frame1"]->date) < $lastDatabaseValue) {
@@ -62,7 +64,7 @@ if($date == date("Y-m-d") && ($database->lastDataset() + Config::getInstance()->
 	}
 	catch (Exception $e) {
 		if($e->getMessage() != "Another process is accessing the bl-net!") {
-			return "{'error:'".$e->getMessage()."'}";
+			return "{'error':'".$e->getMessage()."'}";
 		}
 	}
 }
