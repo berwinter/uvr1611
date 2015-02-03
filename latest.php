3ede39b19396cd1ceb951014ad9d4784642af616
sony <sony@riseup.net>
2015-02-03 09:37:36 +0100

    added hargassner values to latest uvrValues.

--- latest.php
+++ latest.php
@@ -1,7 +1,7 @@
 <?php
 include_once("lib/backend/uvr1611-connection.inc.php");
-include_once("lib/backend/piko-connection.inc.php");
-
+//include_once("lib/backend/piko-connection.inc.php");
+include_once ("/var/www/myUvr1611DataLogger/lib/backend/database.inc.php");
 header('Cache-Control: no-cache, must-revalidate');
 header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
 header('Content-type: application/json; charset=utf-8');
@@ -28,30 +28,14 @@ if(!$data)
 		if ($debug > 0) {
 			echo "latest.php - No connection to BL-Net --> UVR1611!\n";	
 		}
-	}	
-	//PIKO
-	try{
-		$piko = Piko5::getInstance();				
-		if ($piko->fetchData()){			
-			 $myAData = $piko-> getArrValues();			
-			 $frame = $myAData["frame"];
-			/* must be convertet to string, 
-			   otherwise in the schema the values will not be shown */				 
-			 $gdata[$frame] = $myAData;
-			if ($debug > 0) {
-				echo "latest.php - connection to PIKO!\n";	
-			}		
-		} else {
-			if ($debug > 0) {
-				echo "latest.php - No connection to PIKO!\n";	
-			}		
-		}
-	}	
-	catch (Exception $e) {
-		if ($debug > 0) {
-			echo "latest.php - No connection to PIKO!\n";	
-		}		
 	}
+	//Hargassner
+	// connect to database
+	$database = Database::getInstance ();
+	$hgdata = $database->lastHgDataset();
+	$tmpdata = array_merge($gdata["frame1"], $hgdata);
+	$gdata["frame1"] = $tmpdata;
+		
 	
 	$data = json_encode($gdata);
 	save_cache($data,"uvr1611_latest");
